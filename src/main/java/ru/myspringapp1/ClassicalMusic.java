8f2fb3106b304ad6fd7559c6e0e7aaa5f815520a
Ekaterina Bakanova <k125598@yandex.ru>
2021-01-08 16:31:00 +0300

    Annotation @Component.

--- src/main/java/ru/myspringapp1/ClassicalMusic.java
+++ src/main/java/ru/myspringapp1/ClassicalMusic.java
@@ -1,22 +1,12 @@
 package ru.myspringapp1;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class ClassicalMusic implements Music {
-
-    private ClassicalMusic() {}
-
-    public static ClassicalMusic getClassicalMusic() {
-        return new ClassicalMusic();
-    }
 
     @Override
     public String getSong() {
         return "Hungarian Rhapsody";
-    }
-    public void doMyInit() {
-        System.out.println("Initialisation");
-    }
-
-    public void doMyDestroy() {
-        System.out.println("Destraction");
     }
 }
--- src/main/java/ru/myspringapp1/TestSpring.java
+++ src/main/java/ru/myspringapp1/TestSpring.java
@@ -6,27 +6,13 @@ public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationcontext.xml");
 
-        // Music music = context.getBean("musicBean", Music.class);
+        Music music = context.getBean("rockMusic", Music.class);
+        MusicPlayer musicPlayer = new MusicPlayer(music);
+        musicPlayer.playMusic();
 
-        //MusicPlayer musicPlayer = new MusicPlayer(music);
-
-        //MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-
-        //MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-
-        //boolean comp = firstMusicPlayer==secondMusicPlayer;
-
-        //System.out.println(comp);
-        //System.out.println(firstMusicPlayer);
-        //System.out.println(secondMusicPlayer);
-
-        //firstMusicPlayer.setVolume(10);
-        //System.out.println(firstMusicPlayer.getVolume());
-        //System.out.println(secondMusicPlayer.getVolume());
-
-        ClassicalMusic classicalMusic = context.getBean("musicBean", ClassicalMusic.class);
-        System.out.println(classicalMusic.getSong());
-
+        Music music2 = context.getBean("classicalMusic", Music.class);
+        MusicPlayer musicPlayer1 = new MusicPlayer(music2);
+        musicPlayer1.playMusic();
 
         context.close();
     }
